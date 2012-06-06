354c5988ae2c0b1b31b81e3515d59ef2c0dfb7f4
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-06 08:01:37 +0000

    Upgrade widget plugin framework. Settings minor tweaks, tagging version 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/readability-buttons/tags/2.0.2@553720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readability-buttons.php
+++ readability-buttons.php
@@ -3,7 +3,7 @@
 Plugin Name: Readability Buttons (readability.com)
 Plugin URI: http://sparanoid.com/lab/readability-widget/
 Description: Add readability.com widget for your site.
-Version: 2.0
+Version: 2.0.2
 Author: Tunghsiao Liu
 Author URI: http://sparanoid.com/
 Author Email: info@sparanoid.com
@@ -36,29 +36,37 @@ class Readability_Widget extends WP_Widget {
 	 * the widget, loads localization files, and includes necessary scripts and
 	 * styles.
 	 */
-  	// TODO: This should match the title given in the class definition above.
 	public function __construct() {
 	
+    // TODO be sure to change 'widget-name' to the name of *your* plugin
 		load_plugin_textdomain( 'readability_buttons', false, plugin_dir_path( __FILE__ ) . '/lang/' );
 		
+		register_activation_hook( __FILE__, array( &$this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
+		
 		// TODO: update classname and description
-    	// TODO: replace 'plugin-name-locale' to be named more plugin specific. other instances exist throughout the code, too.
+  	// TODO: replace 'widget-name-locale' to be named more plugin specific. other instances exist throughout the code, too.
 		parent::__construct(
 			'widget-readability-buttons-id',
 			'Readability Buttons',
 			array(
 				'classname'		=>	'widget-readability-buttons',
-				'description'	=>	__( 'Short description.', 'plugin-name-locale' )
+				'description'	=>	__( 'Short description.', 'widget-name-locale' )
 			)
 		);
 		
-		// Load JavaScript and stylesheets
-		$this->register_scripts_and_styles();
+		// Register admin styles and scripts
+		add_action( 'admin_print_styles', array( &$this, 'register_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
 	
+		// Register site styles and scripts
+		add_action( 'wp_enqueue_scripts', array( &$this, 'register_widget_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( &$this, 'register_widget_scripts' ) );
+		
 	} // end constructor
 
 	/*--------------------------------------------------*/
-	/* API Functions
+	/* Widget API Functions
 	/*--------------------------------------------------*/
 	
 	/**
@@ -67,7 +75,7 @@ class Readability_Widget extends WP_Widget {
 	 * @args			The array of form elements
 	 * @instance		The current instance of the widget
 	 */
-	function widget( $args, $instance ) {
+	public function widget( $args, $instance ) {
 	
 		extract( $args, EXTR_SKIP );
     $title 		           = apply_filters('widget_title', $instance['title']);
@@ -96,7 +104,7 @@ class Readability_Widget extends WP_Widget {
 	 * @new_instance	The previous instance of values before the update.
 	 * @old_instance	The new instance of values to be generated via the update.
 	 */
-	function update( $new_instance, $old_instance ) {
+	public function update( $new_instance, $old_instance ) {
 		
 		$instance = $old_instance;
 		
@@ -119,7 +127,7 @@ class Readability_Widget extends WP_Widget {
 	 *
 	 * @instance	The array of keys and values for the widget.
 	 */
-	function form( $instance ) {
+	public function form( $instance ) {
 	
     	// TODO define default values for your variables
 		//$instance = wp_parse_args(
@@ -148,62 +156,72 @@ class Readability_Widget extends WP_Widget {
 	} // end form
 	
 	/*--------------------------------------------------*/
-	/* Private Functions
+	/* Public Functions
 	/*--------------------------------------------------*/
-  
-	/**
-	 * Registers and enqueues stylesheets for the administration panel and the
-	 * public facing site.
-	 */
-	private function register_scripts_and_styles() {
 	
-		if ( is_admin() ) {
-		
-			// TODO remember to rename 'widget-slug-' to the name of the widget
-			$this->load_file( 'widget-slug-admin-script', '/js/admin.js', true );
-			$this->load_file( 'widget-slug-admin-style', '/css/admin.css' );
-			
-		} else { 
-		
-			// TODO remember to rename 'widget-slug-' to the name of the widget
-			$this->load_file( 'widget-slug-script', '/js/widget.js', true );
-			$this->load_file( 'widget-slug-style', '/css/widget.css' );
-			
-		} // end if/else
-		
-	} // end register_scripts_and_styles
-
 	/**
-	 * Helper function for registering and enqueueing scripts and styles.
+	 * Fired when the plugin is activated.
 	 *
-	 * @name	The 	ID to register with WordPress
-	 * @file_path		The path to the actual file
-	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
+	 * @params	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
 	 */
-	private function load_file( $name, $file_path, $is_script = false ) {
-		
-		$url = plugins_url( $file_path, __FILE__ ) ;
-		$file = plugin_dir_path( __FILE__ ) . $file_path;
-
-		if( file_exists( $file ) ) {
-		
-			if( $is_script ) {
-			
-				wp_register_script( $name, $url, array( 'jquery' ) );
-				wp_enqueue_script( $name );
-				
-			} else {
-			
-				wp_register_style( $name, $url );
-				wp_enqueue_style( $name );
-				
-			} // end if
-			
-		} // end if
-    
-	} // end load_file
+	public function activate( $network_wide ) {
+		// TODO define activation functionality here
+	} // end activate
 	
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @params	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function deactivate( $network_wide ) {
+		// TODO define deactivation functionality here		
+	} // end deactivate
+	
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public function register_admin_styles() {
+	
+		// TODO change 'widget-name' to the name of your plugin
+		wp_register_script( 'widget-name-admin-styles', plugins_url( 'readability-buttons/css/admin.css' ) );
+		wp_enqueue_script( 'widget-name-admin-styles' );
+			
+	} // end register_admin_styles
+
+	/**
+	 * Registers and enqueues admin-specific JavaScript.
+	 */
+	public function register_admin_scripts() {
+	
+		// TODO change 'widget-name' to the name of your plugin
+		wp_register_script( 'widget-name-admin-script', plugins_url( 'readability-buttons/js/admin.js' ) );
+		wp_enqueue_script( 'widget-name-admin-script' );
+	
+	} // end register_admin_scripts
+	
+	/**
+	 * Registers and enqueues widget-specific styles.
+	 */
+	public function register_widget_styles() {
+	
+		// TODO change 'widget-name' to the name of your plugin
+		wp_register_script( 'widget-name-widget-styles', plugins_url( 'readability-buttons/css/widget.css' ) );
+		wp_enqueue_script( 'widget-name-widget-styles' );
+	
+	} // end register_widget_styles
+	
+	/**
+	 * Registers and enqueues widget-specific scripts.
+	 */
+	public function register_widget_scripts() {
+	
+		// TODO change 'widget-name' to the name of your plugin
+		wp_register_script( 'widget-name-widget-script', plugins_url( 'readability-buttons/js/widget.js' ) );
+		wp_enqueue_script( 'widget-name-widget-script' );
+	
+	} // end register_widget_scripts
+		
 } // end class
-// TODO remember to change 'Readability_Widget' to match the class name definition
+// TODO remember to change 'Widget_Name' to match the class name definition
 add_action( 'widgets_init', create_function( '', 'register_widget("Readability_Widget");' ) ); 
 ?>
